6ddfecdd939178462e84a3769299bff138c98e08
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-07-23 11:17:29 +0200

    Modify migrations

--- server/migrations/20250723082553-create-user.js
+++ server/migrations/20250723082553-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -13,7 +13,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Roles',
+          model: 'roles',
           key: 'id',
         },
         onDelete: 'RESTRICT',
@@ -52,6 +52,6 @@ module.exports = {
     await queryInterface.addIndex('users', ['email']);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   },
 };
