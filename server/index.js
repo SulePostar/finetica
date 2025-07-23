40fc0ae0f652ae2da8492d14eedb0e4d4e07eed1
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-07-23 15:02:49 +0200

    Revert port

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { errorMiddleware } = require('./middleware/error.middleware');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
