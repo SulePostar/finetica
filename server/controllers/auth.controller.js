21e56bab461fee4f22794c8a79840c04d02f82c4
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-07-23 17:46:29 +0200

    Update controller so it sets cookie

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,23 +1,32 @@
 const { loginUser, registerUser } = require('../services/auth.service');
 
+const ACCESS_COOKIE_EXPIRATION_MS = 60 * 60 * 1000;
+
 const login = async (req, res, next) => {
   try {
     const loginData = req.body;
     const accessToken = await loginUser(loginData);
-    res.status(200).json({ accessToken });
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: ACCESS_COOKIE_EXPIRATION_MS,
+      sameSite: 'Strict',
+      path: '/',
+    });
+    res.status(200).json({ message: 'Login successful' });
   } catch (err) {
     next(err);
   }
 };
 
-const register = async (req, res) => {
-  const registerData = req.body;
-
-  const success = await authService.registerUser(registerData);
-
-  res.status(200).json({
-    message: `Successfully registered user with email ${registerData.email} on role ${registerData.roleId} `,
-  });
+const register = async (req, res, next) => {
+  try {
+    const registerData = req.body;
+    const newUser = await registerUser(registerData);
+    res.status(201).json({ newUser, message: 'User registered successfully' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
