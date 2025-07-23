664c9b5eb92be5b36e1da000ddae9ac6ee26eb40
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-07-23 18:20:38 +0200

    Add authentication middleware

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { login, register, logout } = require('../controllers/auth.controller');
 const { validationMiddleware } = require('../middleware/validation.middleware');
 const { loginSchema, registerSchema } = require('../schemas/auth.schemas');
+const { authenticationMiddleware } = require('../middleware/authentication.middleware');
 
 const router = express.Router();
 
 router.post('/login', validationMiddleware(loginSchema), login);
 router.post('/register', validationMiddleware(registerSchema), register);
-router.post('/logout', logout);
+router.post('/logout', authenticationMiddleware, logout);
 
 module.exports = router;
