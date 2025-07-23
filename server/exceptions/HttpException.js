2d27d2c5af05d084c35962f02c68fb613849c599
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-07-23 18:31:45 +0200

    Add basic tests

--- server/exceptions/HttpException.js
+++ server/exceptions/HttpException.js
@@ -1,7 +1,11 @@
-export class HttpException extends Error {
+class HttpException extends Error {
   constructor(status, message) {
     super(message);
     this.status = status;
     this.message = message;
   }
 }
+
+module.exports = {
+  HttpException,
+};
