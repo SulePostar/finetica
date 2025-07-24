b362e4a8901aea61e08cec6b5f64a2cd1db69657
lamijakisic <lamija.kisic1@gmail.com>
2025-07-24 10:09:31 +0200

    login update

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,43 +1,44 @@
-import LoginForm from './../../components/Login/LoginForm'
-import logo from './../../assets/images/Symphony_transparent_1.png'
+import React from 'react';
+import LoginForm from './../../components/Login/LoginForm';
+import logo from './../../assets/images/Symphony_transparent_1.png';
 
 const LoginPage = () => {
     return (
         <div
             className="min-vh-100 d-flex"
-            style={{
-                background: 'linear-gradient(to right, #dbd9e3ff, #a294ebff)',
-            }}
-        >
-            <div
-                className="text-white d-none d-md-flex flex-column justify-content-up align-items-start p-5"
-                style={{ flex: 0.9, backgroundColor: '#8367deff' }}
-            >
 
+        >
+            {/* Left Logo & Text Section */}
+            <div
+                className="text-white d-none d-md-flex flex-column align-items-center text-center p-5"
+                style={{ flex: 1.8, backgroundColor: '#8367deff' }}
+            >
 
                 <img
                     src={logo}
                     alt="Symphony Logo"
                     style={{
-                        width: '500px',
-                        maxWidth: '500%',
-                        marginBottom: '6rem',
+                        width: '700px',
+                        maxWidth: '700%',
+                        marginBottom: '2rem',
                     }}
                 />
 
-                <h1 className="fw-bold">Finetica</h1>
-                <p className="mt-2">Welcome to Symphony's Finetica!</p>
+                <h1 className="fw-bold fs-1">Finetica</h1>
+                <p className="mt-2 fs-4">Welcome to Symphony's Finetica!</p>
+
             </div>
 
-            {/* Right Form */}
+            {/* Right Login Form Section */}
             <div
-                className="bg-white d-flex align-items-center justify-content-center p-5"
-                style={{ flex: 1.6 }}
+                className="d-flex align-items-center justify-content-center p-5"
+                style={{ flex: 1.8, backgroundColor: '#ffffffff' }} // svijetlo ljubičasta
             >
+
                 <LoginForm />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default LoginPage
+export default LoginPage;
