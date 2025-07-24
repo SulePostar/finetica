98713225ec86d447d0202e73045175558d25d34e
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-07-24 11:59:14 +0200

    Add route definitions for Home and About views

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,3 +1,17 @@
-const routes = [];
+import Home from './views/Home';
+import About from './views/About';
+
+const routes = [
+    //   {
+    //     path: '/',
+    //     name: 'Home',
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: '/about',
+    //     name: 'About',
+    //     element: <About />,
+    //   },
+];
 
 export default routes;
