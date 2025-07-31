26f10c675350e02ca45077fc9be8a31f1f477223
ahadziabdi1 <ahadziabdi1@etf.unsa.ba>
2025-07-31 15:01:16 +0200

    Refactor DefaultLayout to use Bootstrap grid system for improved responsiveness

--- client/src/layout/DefaultLayout.jsx
+++ client/src/layout/DefaultLayout.jsx
@@ -1,17 +1,22 @@
+import { Container, Row, Col } from 'react-bootstrap';
 import { AppHeader, AppSidebar } from '../components/index';
 import KIFExampleTable from '../components/Tables/KIFExampleTable';
 
 const DefaultLayout = () => {
   return (
-    <div className="d-flex flex-row min-vh-100 bg-light dark:bg-dark">
-      <AppSidebar />
-      <div className="wrapper d-flex flex-column flex-grow-1">
-        <AppHeader />
-        <main className="p-3 flex-grow-1">
-          <KIFExampleTable />
-        </main>
-      </div>
-    </div>
+    <Container fluid className="min-vh-100 bg-light dark:bg-dark">
+      <Row className="flex-nowrap">
+        <Col xs="auto" className="p-0">
+          <AppSidebar />
+        </Col>
+        <Col className="d-flex flex-column p-0">
+          <AppHeader />
+          <Container as="main" className="p-3 flex-grow-1">
+            <KIFExampleTable />
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
