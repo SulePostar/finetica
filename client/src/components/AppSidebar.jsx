524de38abf778435bdc3e1d1b69efec2b4c4642f
Ridvan77 <ridvanzolja122@gmail.com>
2025-08-04 19:04:33 +0200

    updated sidebar for theme change

--- client/src/components/AppSidebar.jsx
+++ client/src/components/AppSidebar.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import CIcon from '@coreui/icons-react';
 import {
-  CCloseButton,
   CSidebar,
   CSidebarBrand,
   CSidebarFooter,
   CSidebarHeader,
   CSidebarToggler,
+  CCloseButton,
 } from '@coreui/react';
 
 import { AppSidebarNav } from './AppSidebarNav';
-
 import navigation from '../_nav';
 
 const AppSidebar = () => {
@@ -20,22 +18,38 @@ const AppSidebar = () => {
   const unfoldable = useSelector((state) => state.sidebarUnfoldable);
   const sidebarShow = useSelector((state) => state.sidebarShow);
 
+  const [isDarkMode, setIsDarkMode] = useState(
+    document.documentElement.getAttribute('data-coreui-theme') === 'dark'
+  );
+
+  useEffect(() => {
+    const handler = () => {
+      const theme = document.documentElement.getAttribute('data-coreui-theme');
+      setIsDarkMode(theme === 'dark');
+    };
+
+    document.documentElement.addEventListener('ColorSchemeChange', handler);
+    return () => document.documentElement.removeEventListener('ColorSchemeChange', handler);
+  }, []);
+
+
   return (
     <CSidebar
-      className="border-end"
-      colorScheme="dark"
+      className='border-end'
+      colorScheme={isDarkMode ? 'dark' : 'light'}
       position="fixed"
       unfoldable={unfoldable}
       visible={sidebarShow}
       onVisibleChange={(visible) => dispatch({ type: 'set', sidebarShow: visible })}
     >
       <CSidebarHeader className="border-bottom">
-        <CSidebarBrand to="/">
+        <CSidebarBrand to="/" className="text-decoration-none">
+          {/* Logo position */}
         </CSidebarBrand>
 
         <CCloseButton
           className="d-lg-none"
-          dark
+          dark={isDarkMode}
           onClick={() => dispatch({ type: 'set', sidebarShow: false })}
         />
       </CSidebarHeader>
