e7d396be51ede0660ce732d5b60353412c178ead
Maxed31 <armin.buza@stu.ibu.edu.ba>
2025-08-05 14:13:05 +0200

    sidebar animation implementation

--- client/src/store.js
+++ client/src/store.js
@@ -3,23 +3,49 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import userReducer from './redux/user/userSlice';
-import authReducer from './redux/auth/authSlice'; 
+import authReducer from './redux/auth/authSlice';
 
+// Simple UI state reducer
+const initialUIState = {
+  sidebarShow: true,
+  sidebarUnfoldable: false,
+  headerShow: true,
+};
+
+const uiReducer = (state = initialUIState, action) => {
+  if (action.type === 'set') {
+    return { ...state, ...action };
+  }
+  return state;
+};
 
 // Root reducer: auth, user (persistirani) + ui (nije persistiran)
-const rootReducer = combineReducers({
-  user: userReducer,
-  auth: authReducer,
-});
+const rootReducer = (state, action) => {
+  return combineReducers({
+    user: userReducer,
+    auth: authReducer,
+  })(state, action);
+};
+
+// Enhanced root reducer with UI state
+const enhancedRootReducer = (state, action) => {
+  const combinedState = rootReducer(state, action);
+  const uiState = uiReducer(state, action);
+
+  return {
+    ...combinedState,
+    ...uiState,
+  };
+};
 
 // Konfiguracija za redux-persist
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['user', 'auth'] // samo user i auth se čuvaju u localStorage
+  whitelist: ['user', 'auth'], // samo user i auth se čuvaju u localStorage, UI state nije persistovan
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, enhancedRootReducer);
 
 // Store konfiguracija
 export const store = configureStore({
@@ -29,7 +55,7 @@ export const store = configureStore({
       serializableCheck: {
         ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
       },
-    })
+    }),
 });
 
 // Persistor (za redux-persist)
