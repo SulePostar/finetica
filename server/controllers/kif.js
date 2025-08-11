41f7ae16374a09c5149f8171f19b89ead6de5678
enahamzic# <enahamzic16@gmail.com>
2025-08-11 13:33:07 +0200

    Merging with master

--- server/controllers/kif.js
+++ server/controllers/kif.js
@@ -1,20 +1,5 @@
 const { tr } = require('zod/v4/locales');
-const { getPaginatedKifData } = require('../services/kif');
-const { fetchKifById } = require('../services/kif');
-
-const getKifById = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const item = fetchKifById(id);
-        if (!item) {
-            return res.status(404).json({ error: 'Item not found' });
-        }
-        res.json(item);
-    } catch (err) {
-        console.error('Error fetching item:', err);
-        return res.status(500).json({ error: 'Internal server error' });
-    }
-};
+const { getPaginatedKifData} = require('../services/kif');
 
 const getKifData = (req, res) => {
     const { page, perPage, sortField, sortOrder } = req.query;
@@ -31,5 +16,4 @@ const getKifData = (req, res) => {
 
 module.exports = {
     getKifData,
-    getKifById
 };
--- server/routes/kif.js
+++ server/routes/kif.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getKifData } = require('../controllers/kif');
+const { getKifData} = require('../controllers/kif');
 
 router.get('/kif-data', getKifData);
 
