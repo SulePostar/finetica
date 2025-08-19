8e1009adff21b4d2933469dcc3b15668f0d3e67f
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-08-19 11:12:16 +0200

    Expose route to approve contracts

--- server/routes/contract.js
+++ server/routes/contract.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getContractData } = require('../controllers/contract');
+const { getContractData, approveContract } = require('../controllers/contract');
+const isAuthenticated = require('../middleware/isAuthenticated');
+const validate = require('../middleware/validation');
+
+const approveContractSchema = require('../schemas/approveContract');
 
 router.get('/', getContractData);
+router.put('/:id/approve', isAuthenticated, validate(approveContractSchema), approveContract);
 
 module.exports = router;
