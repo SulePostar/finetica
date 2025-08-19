ee91bc571ad87fce00d445930624e15435441231
aminaabdibegovic <aminaabdibegovic005@gmail.com>
2025-08-19 13:22:01 +0200

    Remove route and controller for createContracts

--- server/routes/contract.js
+++ server/routes/contract.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getContractData, approveContract, getContract, addContract } = require('../controllers/contract');
+const { getContractData, approveContract, getContract } = require('../controllers/contract');
 const isAuthenticated = require('../middleware/isAuthenticated');
 const validate = require('../middleware/validation');
 
@@ -8,7 +8,6 @@ const approveContractSchema = require('../schemas/approveContract');
 
 router.get('/', getContractData);
 router.put('/:id/approve', isAuthenticated, validate(approveContractSchema), approveContract);
-router.post('/', isAuthenticated, addContract);
 router.get('/:id', isAuthenticated, getContract);
 
 module.exports = router;
