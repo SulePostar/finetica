5b52c86d7972b67e50bdfe8c6c24d3af58438e0a
vgrbo1 <vgrbo1@etf.unsa.ba>
2025-08-19 13:19:07 +0200

    Fix add contract

--- server/routes/contract.js
+++ server/routes/contract.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getContractData, approveContract, getContract } = require('../controllers/contract');
+const { getContractData, approveContract, getContract, addContract } = require('../controllers/contract');
 const isAuthenticated = require('../middleware/isAuthenticated');
 const validate = require('../middleware/validation');
 
