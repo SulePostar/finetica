74e0d5a2436a1ab5c82ce0d09c9a2d9b07e8a427
Vedad Grbo <92470181+vgrbo1@users.noreply.github.com>
2025-08-26 15:55:28 +0200

    Feature/contract processing (#89)
    * Remove duplicate imports
    
    * Refactor DocumentInfo and InvoiceDetails components; update field keys in InvoicesData and improve contract approval logic
    
    * Add contract schema
    
    * Add contracts prompt
    
    * Add method to extract data from files
    
    * Implement data extraction
    
    * Add new migration and model contract_processed_files
    
    * Refactor model for tracking processing of files
    
    * Add fetching unprocessed files
    
    * Add fetching file from supabase
    
    * feat: Update contract schema to use partnerId and enhance data extraction with available business partners
    
    * Add getFile method for supabase
    
    * Add route for PDF generation, integrate AI model for extracting data from PDF into database
    
    * Fix logger
    
    * Add processing of unprocessed files
    
    * Remove console log
    
    * Expose route to process files
    
    * Change route
    
    * Add processing of files
    
    * Refactor processUnprocessedFiles function
    
    * Remove export
    
    * Update method to process files
    
    * Remove contract generation script
    
    * Remove library for generating pdf documents
    
    * Remove dead code
    
    * Remove unnecessary service
    
    * Rename model file
    
    * Remove unecessary controller and route
    
    ---------
    
    Co-authored-by: aminaabdibegovic <aminaabdibegovic005@gmail.com>
    Co-authored-by: ahadziabdi1 <ahadziabdi1@etf.unsa.ba>

--- server/routes/contract.js
+++ server/routes/contract.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { getContractData, approveContract, getContract } = require('../controllers/contract');
 const isAuthenticated = require('../middleware/isAuthenticated');
 const validate = require('../middleware/validation');
-
 const approveContractSchema = require('../schemas/approveContract');
 
 router.get('/', getContractData);
