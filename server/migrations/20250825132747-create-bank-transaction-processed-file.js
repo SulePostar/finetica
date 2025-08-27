73ce0a95683fc1874bce7fa91048ce0ca2283e71
enahamzic# <enahamzic16@gmail.com>
2025-08-27 11:25:32 +0200

    Renaming

--- server/migrations/20250825132747-create-bank-transaction-processed-file.js
+++ server/migrations/20250825132747-create-bank-transaction-processed-file.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('bank_transaction_processed_files', {
+    await queryInterface.createTable('bank_transaction_processing_logs', {
       id: {
         type: Sequelize.INTEGER,
         autoIncrement: true,
@@ -40,6 +40,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('bank_transaction_processed_files');
+    await queryInterface.dropTable('bank_transaction_processing_logs');
   }
 };
