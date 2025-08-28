00715a4a64e03a5662ef2feac28f37f5faa0f847
Ena Hamzic <146780088+enahamzic@users.noreply.github.com>
2025-08-28 14:25:59 +0200

    Feat/transactions ai supabase sync (#88)
    * Added ProcessedKifBucketFiles, cliHelper, fileTracker and kifProcessor
    
    * Env file loading fix
    
    * Start working on new feature
    
    * Fix1
    
    * Optimisations and redundancy improved
    
    * Changed a migration
    
    * Make a new file processBankTransactionBucketFiles.js in order to process transaction files, list all files, and so on
    
    * Finished task
    
    * Changed a file correcting the transactionProcessor instead of processor variable
    
    * Refactored task for processing files
    
    * Deleted redundant files
    
    * Updated implementation
    
    * Refactored code
    
    * Delete .DS_Store
    
    * Removing dead code and some changes related to association (removing 'required: false')
    
    * Including isAuthenticated middleware in /process route for processing documents from supabase
    
    * Cleaning up code
    
    * Cleaning up code
    
    * Cleaning up code
    
    * Finished task
    
    * Renaming
    
    * Renaming files
    
    * Modified service and controller in order to control and process bank transactions
    
    * Removed unused methods
    
    * Resolving conflicts in BankTransactions.css
    
    * Removed unused methods, changed logic for processing files
    
    * Added a new task for processing file
    
    * Introduced a new task which will be used to process documents downloaded from google drive or sopabase
    
    * Removed a seeder
    
    * Changed validation schema for easy approving bank transaction file
    
    ---------
    
    Co-authored-by: HajrudinRed <hajrudin.bajraktarevic@stu.ibu.edu.ba>
    Co-authored-by: FikretZajmovic <fikret.zajmovic@stu.ibu.edu.ba>
    Co-authored-by: farisgigic <faris.gigic@yahoo.com>
    Co-authored-by: Faris <147379644+farisgigic@users.noreply.github.com>

--- server/controllers/bankTransaction.js
+++ server/controllers/bankTransaction.js
@@ -2,14 +2,12 @@ const {
     getTransactions,
     getBankTransactionById,
     createBankTransactionManually,
-    approveBankTransaction,
-    editBankTransaction,
-    processBankTransaction
+    approveBankTransactionById,
+    editBankTransaction
 } = require('../services/bankTransaction');
 
 const getBankTransactions = async (req, res, next) => {
     try {
-        // Pass the whole query object directly to the service
         const result = await getTransactions(req.query);
         res.json(result);
     } catch (error) {
@@ -22,23 +20,16 @@ const getTransactionById = async (req, res, next) => {
     try {
         const { id } = req.params;
         const document = await getBankTransactionById(id);
-
-        if (!document) {
-            return res.status(404).json({ error: 'Bank Transaction document not found' });
-        }
-
         res.json(document);
     } catch (error) {
         next(error);
     }
 };
 
-
 const createBankTransaction = async (req, res) => {
     try {
         const transactionData = req.body;
         const userId = req.user.userId;
-
         const result = await createBankTransactionManually(transactionData, userId);
         res.status(201).json(result);
     } catch (error) {
@@ -47,20 +38,10 @@ const createBankTransaction = async (req, res) => {
     }
 };
 
-const processTransaction = async (req, res, next) => {
-    try {
-        const { model } = req.body;
-        const result = await processBankTransaction(req.file.buffer, req.file.mimetype, model);
-        res.json(result);
-    } catch (error) {
-        next(error);
-    }
-};
-
 const approveTransaction = async (req, res) => {
     const { id: transactionId } = req.params;
     const { userId } = req.user;
-    const result = await approveBankTransaction(transactionId, userId, req.body);
+    const result = await approveBankTransactionById(transactionId, userId, req.body);
     res.json(result);
 };
 
@@ -77,12 +58,11 @@ const updatedDocument = async (req, res) => {
     }
 }
 
+
 module.exports = {
     getBankTransactions,
     getTransactionById,
-    processTransaction,
     createBankTransaction,
     approveTransaction,
-    updatedDocument,
-    editBankTransaction
+    updatedDocument
 };
