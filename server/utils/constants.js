50002be7cccf00fe12678235799660591ebdc1fe
farisgigic <faris.gigic@yahoo.com>
2025-08-29 00:05:11 +0200

    Modified task processBankTransactionFile in order to fetch both google drive and supabase files and process them

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -5,6 +5,25 @@ const USER_STATUS = Object.freeze({
   DELETED: 4,
 });
 
+const SOURCES = {
+  kif: {
+    localFolder: './googleDriveDownloads/kif/',
+    supabaseBucket: 'kif'
+  },
+  kuf: {
+    localFolder: './googleDriveDownloads/kuf/',
+    supabaseBucket: 'kuf'
+  },
+  transactions: {
+    localFolder: './googleDriveDownloads/bank-transactions/',
+    supabaseBucket: 'transactions'
+  },
+  contracts: {
+    localFolder: './googleDriveDownloads/contracts/',
+    supabaseBucket: 'contracts'
+  }
+};
 module.exports = {
   USER_STATUS,
+  SOURCES
 };
---
+++ server/utils/fileFetcher.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+const supabaseService = require('./supabase/supabaseService');
+const { SOURCES } = require('./constants');
+
+/**
+ * Fetch file from either local folder or supabase bucket
+ * @param {string} category - one of 'kif', 'kuf', 'transactions', 'contracts'
+ * @param {string} fileName - name of file to fetch
+ * @param {string} source - 'local' or 'supabase'
+ */
+
+async function getFileSource(category, fileName, source = 'local') {
+    const config = SOURCES[category];
+    if (!config) {
+        throw new Error(`Unknown category: ${category}`);
+    }
+
+    if (source === 'local') {
+        const filePath = path.join(config.localFolder, fileName);
+        const buffer = fs.readFileSync(filePath);
+        const mimeType = mime.getType(filePath) || 'application/pdf';
+        return { buffer, mimeType, fileName, category };
+    }
+
+    if (source === 'supabase') {
+        const { buffer, mimeType } = await supabaseService.getFile(
+            config.supabaseBucket,
+            fileName
+        );
+        return { buffer, mimeType, fileName, category };
+    }
+
+    throw new Error(`Unknown file source: ${source}`);
+}
+
+module.exports = { getFileSource };
