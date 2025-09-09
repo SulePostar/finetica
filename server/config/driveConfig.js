23d9b46661d9b84bf0ad023e8ed8642cd3d7c404
Faris <147379644+farisgigic@users.noreply.github.com>
2025-09-09 13:32:29 +0200

    [Fix] Google Drive integration  (#121)
    * Changed logic for connecting project files with Google Drive
    
    * Little fixes2
    
    * Little tweaks again
    
    * Changing the logic with fetching from .env

--- server/config/driveConfig.js
+++ server/config/driveConfig.js
@@ -1,21 +1,21 @@
-const { google } = require('googleapis'); // oficijelna biblioteka za Google API
+const { google } = require('googleapis');
+
 const dotenv = require('dotenv');
 dotenv.config();
 
-const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-);
+const GOOGLE_SERVICE_ACCOUNT_KEY = process.env.GOOGLE_SERVICE_ACCOUNT_KEY;
+const credentials = JSON.parse(GOOGLE_SERVICE_ACCOUNT_KEY);
+
+const auth = new google.auth.GoogleAuth({
+    credentials,
+    scopes: ['https://www.googleapis.com/auth/drive.readonly'], // adjust scope if needed
+});
 
 function createDriveClient() {
     return google.drive({
-        version: 'v3', // najnovija verzija Google Drive API-ja 
-        auth: oauth2Client, // koristi se prethodno definisani OAuth2 klijent za autentifikovane pozive
+        version: 'v3',
+        auth,
     });
 }
 
-module.exports = {
-    oauth2Client,
-    createDriveClient,
-};
+module.exports = { createDriveClient };
