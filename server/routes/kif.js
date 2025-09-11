ba1086f31f001592b5cc820d74fbb675b8f76d05
FikretZajmovic <147069935+FikretZajmovic@users.noreply.github.com>
2025-09-11 11:11:49 +0200

    Feature/kif manual validation (#124)
    * Created kif processing log service and controller for invalid kif files functions
    
    * Added kif routes for getting invalid kifs and kif log
    
    * Updated uploaded files service with kif invalid files flag and added invalid message handle for all file types
    
    * Modified kif service for handling invalid files

--- server/routes/kif.js
+++ server/routes/kif.js
@@ -14,6 +14,10 @@ const {
     kifInvoiceCreateSchema,
     kifInvoiceUpdateSchema,
 } = require('../schemas/kifJoiSchema');
+const { getInvalidKifs, getKifLog } = require('../controllers/kifProcessingLog');
+
+router.get('/logs/invalid', isAuthenticated, getInvalidKifs);
+router.get('/logs/:id',   isAuthenticated, getKifLog);
 
 router.get('/', getKifData);
 router.get('/:id', getKif);
