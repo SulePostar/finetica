9bde586cbc16daa98d4744ab01f8d4d8b9c3cedb
Danijela Ikanović <danijela.ikanovic@symphony.is>
2025-11-19 09:03:32 +0100

    Kif page create

---
+++ frontend/src/pages/Kif.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DefaultLayout from "@/layout/DefaultLayout";
+
+const Kif = () => {
+    return (
+        <DefaultLayout>
+            <h1>Kif Page</h1>
+        </DefaultLayout>
+    );
+}
+
+export default Kif;
