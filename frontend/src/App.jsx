751cf7b8d56b0eda51accd6e9b6c2356885aa997
Ena Hamzic <146780088+enahamzic@users.noreply.github.com>
2025-11-19 11:49:44 +0100

    Feature/simple sidebar (#204)
    * Fixed tailwind setup
    
    * Removed unused ui components
    
    * Removed debugging changes
    
    * Removed debugging changes again
    
    * Cleaning up PR
    
    * Sidebar addition
    
    * Added sidebar with placeholder values

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
 import { Button } from "@/components/ui/button"
 import AppRoutes from "./routes/AppRoutes"
+import DefaultLayout from "./layout/DefaultLayout"
 
 function App() {
   return (
-    <><AppRoutes /><div className="p-8 space-y-4">
-      <h1 className="text-3xl font-bold">Hello World with Tailwind CSS & shadcn/ui!</h1>
-      <div className="flex gap-4 flex-wrap">
-        <Button onClick={() => alert('Default button clicked')}>Default Button</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="outline">Outline</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="link">Link</Button>
-      </div>
-    </div></>
+    <DefaultLayout>
+      <AppRoutes />
+    </DefaultLayout>
   )
 }
 
