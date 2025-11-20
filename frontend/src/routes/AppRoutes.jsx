9b0931f9ff5449f70837ddfa0db2db04120325da
Amidza-Idris <115904397+Amidza-Idris@users.noreply.github.com>
2025-11-20 10:21:32 +0100

    Created AppRoutes. Left placeholder for register (#212)
    * Created AppRoutes. Left placeholder for register
    and login.
    
    * Removed unused lines
    
    * Removed commented code.
    Removed DefaultLayer import which is not used.
    
    * Removed unused DefaultLayer.

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,63 @@
-import Register from '@/pages/Register';
-import { Routes, Route } from 'react-router-dom'
+import { Navigate, Routes, Route } from 'react-router-dom'
+import React from 'react';
+
+const BankStatements = React.lazy(() => import('../pages/BankStatements'));
+const Kuf = React.lazy(() => import('../pages/Kuf'));
+const Contacts = React.lazy(() => import('../pages/Contacts'));
+const Partner = React.lazy(() => import('../pages/PartnersPage'));
+const Kif = React.lazy(() => import('../pages/Kif'));
+const Register = () => import('../pages/Register');
+
 
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<div>Routing works</div>} />
-            <Route path="/test" element={<div>Test route</div>} />
+            <Route
+                path="/"
+                element={
+                    <h1 className="text-2xl font-semibold" >Home Page Placeholder</h1>
+                }
+            />
+            <Route
+                path="/register"
+                element={
+                    <Register />
+                }
+            />
+            <Route
+                path="/kif"
+                element={
+                    <Kif />
+                }
+            />
+            <Route
+                path="/bank-statements"
+                element={
+                    <BankStatements />
+                }
+            />
+            <Route
+                path="/kuf"
+                element={
+                    <Kuf />
+                }
+            />
+            <Route
+                path="/contacts"
+                element={
+                    <Contacts />
+                }
+            />
+            <Route
+                path="/partners"
+                element={
+                    <Partner />
+                }
+            />
+
+            {/* Fallback for unknown routes */}
+
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     );
 }
