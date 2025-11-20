bd6fd14feba1a7b8b97594130157b93ba2b62d8b
TarikKarahodzic <tariktare@live.com>
2025-11-20 15:33:20 +0100

    Small fix on main card for responsiveness

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -6,7 +6,7 @@ import { Card } from '@/components/ui/card';
 const ProfilePage = () => {
   return (
     <main>
-      <Card className="w-[65vw] mx-auto p-2 w-full">
+      <Card className="lg:w-[55vw] mx-auto p-2 sm:w-full">
         <ProfileHeader />
         <ProfileAvatarSection />
       </Card>
