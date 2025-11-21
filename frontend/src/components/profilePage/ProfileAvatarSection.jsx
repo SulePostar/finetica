68a4ed9e3499ad683ada06d78ba94271bd271d72
TarikKarahodzic <tariktare@live.com>
2025-11-21 11:39:10 +0100

    Added changes from the comment on PR

--- frontend/src/components/profilePage/ProfileAvatarSection.jsx
+++ frontend/src/components/profilePage/ProfileAvatarSection.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
-import { CardDescription, CardHeader, CardTitle } from '../ui/card'
+import { CardContent, CardDescription, CardTitle } from '../ui/card'
 
 const ProfileAvatarSection = () => {
   return (
-    <CardHeader>
+    <CardContent>
       <div className="flex items-center w-full flex-wrap gap-6">
         <div className="">
           <Avatar className="h-24 w-24 lg:h-48 lg:w-48 md:h-42 md:w-42 sm:h-32 sm:w-32">
@@ -18,16 +18,13 @@ const ProfileAvatarSection = () => {
             <span className="inline ml-2 text-sm opacity-70">(role)</span>
           </CardTitle>
 
-
-
           <CardDescription>
             <p className="text-base">john.doe@example.com</p>
             <span className="text-xs">Last login: DD:MM:YYYY hh:mm</span>
           </CardDescription>
         </div>
       </div>
-
-    </CardHeader>
+    </CardContent>
   )
 }
 
