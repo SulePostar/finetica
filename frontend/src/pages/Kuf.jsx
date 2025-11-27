970c891becc3f1aa48b40bf7b2df995fab4ec17a
lenasaraba <lenasaraba8@gmail.com>
2025-11-27 15:07:20 +0100

    Implement API functions for KUF invoices

--- frontend/src/pages/Kuf.jsx
+++ frontend/src/pages/Kuf.jsx
@@ -9,110 +9,17 @@ import { Button } from "@/components/ui/button";
 import { MoreHorizontal } from "lucide-react";
 import DynamicTable from "@/components/table/DynamicTable";
 import PageTitle from "@/components/shared-ui/PageTitle";
+import { useKufInvoices } from "@/queries/Kuf";
 
 const Kuf = () => {
-    const mockKufData = [
-        {
-            id: 1,
-            invoiceNumber: "62320",
-            type: "Invoice",
-            customer: "—",
-            invoiceDate: "08/09/2025",
-            dueDate: "07/12/2025",
-            totalAmount: "52969.00",
-            review: "Rejected",
-        },
-        {
-            id: 2,
-            invoiceNumber: "22222",
-            type: "Faktura",
-            customer: "—",
-            invoiceDate: "13/08/2025",
-            dueDate: "12/09/2025",
-            totalAmount: "714.39",
-            review: "Approved",
-        },
-        {
-            id: 3,
-            invoiceNumber: "40405-0808064-14075",
-            type: "Račun",
-            customer: "—",
-            invoiceDate: "15/07/2025",
-            dueDate: "30/07/2025",
-            totalAmount: "56.34",
-            review: "Approved",
-        },
-        {
-            id: 4,
-            invoiceNumber: "8529",
-            type: "Invoice",
-            customer: "—",
-            invoiceDate: "10/01/2025",
-            dueDate: "24/01/2025",
-            totalAmount: "67329.60",
-            review: "Approved",
-        },
-        {
-            id: 5,
-            invoiceNumber: "12366-P1-1",
-            type: "Račun",
-            customer: "—",
-            invoiceDate: "13/08/2025",
-            dueDate: "—",
-            totalAmount: "54.00",
-            review: "Approved",
-        },
-        {
-            id: 6,
-            invoiceNumber: "250724225755",
-            type: "Račun",
-            customer: "—",
-            invoiceDate: "31/07/2025",
-            dueDate: "08/08/2025",
-            totalAmount: "11.05",
-            review: "Pending",
-        },
-        {
-            id: 7,
-            invoiceNumber: "347003",
-            type: "INVOICE",
-            customer: "—",
-            invoiceDate: "29/03/2025",
-            dueDate: "28/04/2025",
-            totalAmount: "577271.99",
-            review: "Pending",
-        },
-        {
-            id: 8,
-            invoiceNumber: "702016",
-            type: "Invoice",
-            customer: "—",
-            invoiceDate: "08/04/2025",
-            dueDate: "08/05/2025",
-            totalAmount: "1106.06",
-            review: "Pending",
-        },
-        {
-            id: 9,
-            invoiceNumber: "5324027835",
-            type: "Invoice",
-            customer: "—",
-            invoiceDate: "31/07/2025",
-            dueDate: "—",
-            totalAmount: "0.06",
-            review: "Pending",
-        },
-        {
-            id: 10,
-            invoiceNumber: "15997",
-            type: "Invoice",
-            customer: "—",
-            invoiceDate: "26/11/2023",
-            dueDate: "—",
-            totalAmount: "97299.36",
-            review: "Approved",
-        },
-    ];
+
+
+    const { data, isPending } = useKufInvoices();
+
+    console.log("data", data);
+    console.log(data);
+    console.log(isPending);
+
 
     function getKufColumns(onAction) {
         return [
@@ -126,9 +33,9 @@ const Kuf = () => {
                 ),
             },
             {
-                accessorKey: "type",
+                accessorKey: "invoiceType",
                 header: "Type",
-                cell: ({ row }) => row.original.type || "—",
+                cell: ({ row }) => row.original.invoiceType || "—",
             },
             {
                 accessorKey: "customer",
@@ -149,7 +56,7 @@ const Kuf = () => {
                 accessorKey: "totalAmount",
                 header: "Total Amount",
                 cell: ({ row }) => {
-                    const value = row.original.totalAmount;
+                    const value = row.original.netTotal;
                     return value ? `${value} KM` : "—";
                 },
             },
@@ -157,7 +64,9 @@ const Kuf = () => {
                 accessorKey: "review",
                 header: "Review",
                 cell: ({ row }) => {
-                    const value = row.original.review?.toLowerCase();
+                    const approved = row.original.approvedAt || row.original.approvedBy;
+
+                    const value = approved ? 'approved' : 'pending';
 
                     const statusStyles = {
                         approved: "bg-chart-2 text-primary-foreground",
@@ -170,9 +79,9 @@ const Kuf = () => {
 
                     return (
                         <Badge className={color}>
-                            {row.original.review
-                                ? row.original.review.charAt(0).toUpperCase() +
-                                row.original.review.slice(1)
+                            {value
+                                ? value.charAt(0).toUpperCase() +
+                                value.slice(1)
                                 : "—"}
                         </Badge>
                     );
@@ -215,10 +124,11 @@ const Kuf = () => {
         ];
     }
 
+    if (isPending) return <p>"Loading..."</p>
     return (
         <>
             <PageTitle text="KUF - Purchase Invoices" />
-            <DynamicTable columns={getKufColumns((item) => console.log("Action on:", item))} data={mockKufData} />
+            <DynamicTable columns={getKufColumns((item) => console.log("Action on:", item))} data={data.data} />
         </>
     );
 }
