10ca779e90e05625d57aafa0c969f9eedaad4020
lenasaraba <133240741+lenasaraba@users.noreply.github.com>
2025-12-02 13:02:44 +0100

    Refactor Kuf page and add utility functions for formatting data (#243)

---
+++ frontend/src/helpers/capitalizeFirstLetter.js
@@ -0,0 +1,14 @@
+/**
+ * Capitalizes the first character of a string.
+ *
+ * @param {string} str - The input string to be capitalized.
+ * @returns {string} The string with the first character in uppercase.
+ *
+ * @example
+ * capitalizeFirst("user") // returns "User"
+ * capitalizeFirst("") // returns ""
+ */
+export const capitalizeFirst = (str) => {
+    if (!str) return '';
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
