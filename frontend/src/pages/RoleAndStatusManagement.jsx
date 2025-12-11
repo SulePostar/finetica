6d30a73000ffaded7c19f1b53b0fcd20599ee718
lamijakisic <lamija.kisic1@gmail.com>
2025-12-11 10:41:16 +0100

    padding issues fixed

--- frontend/src/pages/RoleAndStatusManagement.jsx
+++ frontend/src/pages/RoleAndStatusManagement.jsx
@@ -15,6 +15,7 @@ export default function RoleAndStatusManagement() {
     if (statusPending || rolesPending) {
         return (
             <>
+                <div className="px-4 md:px-6 lg:px-8"></div>
                 <PageTitle text="Roles and Statuses" />
                 <div className="flex items-center justify-center h-40">
                     <Spinner className="w-10 h-10 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 text-[var(--spurple)]" />
