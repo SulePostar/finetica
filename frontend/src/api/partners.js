780f3b07ceca3422518b5a52279a87e180d9f6df
lenasaraba <lenasaraba8@gmail.com>
2025-12-15 13:42:35 +0100

    Remove debug log from getAllPartners function

--- frontend/src/api/partners.js
+++ frontend/src/api/partners.js
@@ -2,6 +2,5 @@ import apiClient from "./axios";
 
 export const getAllPartners = async (filters) => {
     const { data } = await apiClient.get("/partners", { params: filters });
-    console.log("Fetched partners data:", data);
     return data;
 }
