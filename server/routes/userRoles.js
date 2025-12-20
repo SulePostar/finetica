ae50533bddc6a5352d011d4f19b77438c4fe71c7
dzevadmadzak <dmadzak1@etf.unsa.ba>
2025-12-20 13:25:37 +0100

    added hasRole to the userRoles and userStatus routes

--- server/routes/userRoles.js
+++ server/routes/userRoles.js
@@ -6,17 +6,21 @@ const {
     deleteUserRole,
 } = require('../controllers/userRoles');
 const isAuthenticated = require('../middleware/isAuthenticated');
+const hasRole = require('../middleware/hasRole');
 
 const router = express.Router();
 router.get('/',
     isAuthenticated,
+    hasRole(['admin']),
     getAllUserRoles);
 router.get('/:id', isAuthenticated, getUserRoleById);
 router.post('/',
     isAuthenticated,
+    hasRole(['admin']),
     createUserRole);
 router.delete('/:id',
     isAuthenticated,
+    hasRole(['admin']),
     deleteUserRole);
 
 module.exports = router;
