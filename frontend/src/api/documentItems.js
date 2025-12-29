bb8cb759275c4fba2559b433e5c4e71ad452d604
ImadDumanjic <imad.dumanjic@stu.ibu.edu.ba>
2025-12-29 15:36:25 +0100

    refactor: Remove `API_BASE_URL` constant and `withCredentials` option from `getDocumentItems` API call.

--- frontend/src/api/documentItems.js
+++ frontend/src/api/documentItems.js
@@ -1,11 +1,6 @@
 import apiClient from './axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-export const getDocumentItems = async (type, id) => {
-    const { data } = await apiClient.get(`/${type}/${id}/items`, {
-        withCredentials: true,
-    });
-
+export async function getDocumentItems(type, id) {
+    const { data } = await apiClient.get(`/${type}/${id}/items`);
     return data?.data ?? data;
-};
+}
